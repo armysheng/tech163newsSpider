1a2a0f467e44ce04b18326883513f1f62a553425
Sheng Junhui <armysheng@Gmail.com>
2014-08-13 14:02:59 +0800

    origin

--- tech163/tech163/pipelines.py
+++ tech163/tech163/pipelines.py
@@ -12,24 +12,9 @@ class Tech163Pipeline(object):
         if spider.name != "news":  return item
         if item.get("news_thread", None) is None: return item
 
-        # spec = { "title": item["title"] }
-        # NewsDB.tech163.insert(spec, {'$set': dict(item)}, upsert=True)
-        # NewsDB.tech163.save({'news_title':news_title,'news_time':news_time,'news_from':news_from,'from_url':from_url,'news_body':news_body})
-        # NewsDB.tech163.insert(item)
         spec = { "news_thread": item["news_thread"] }
         NewsDB.news.update(spec, {'$set': dict(item)}, upsert=True)
 
         return None
         
 
-
-
-# class MoviePipeline(object):
-#     def process_item(self, item, spider):
-#         if spider.name != "movie":  return item
-#         if item.get("subject_id", None) is None: return item
-
-#         spec = { "subject_id": item["subject_id"] }
-#         doubanDB.movie.update(spec, {'$set': dict(item)}, upsert=True)
-
-#         return None
